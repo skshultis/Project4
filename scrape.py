62b9c72b8ef4e3b9da0d7e3832b678a7f4ac2587
Sarah K Shultis <skshultis@Sks-MacBook-Air.local>
2016-09-13 12:20:32 -0400

    pushing again before last scrape attempt, looking bleak

--- scrape.py
+++ scrape.py
@@ -1,20 +1,20 @@
 import requests
-from lxml import etree
-import urllib2
+# from lxml import etree
+import urllib
 from bs4 import BeautifulSoup
 
 url = 'http://opendata.dc.gov/datasets/dc3289eab3d2400ea49c154863312434_8?uiTab=table'
 # headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
 # response = requests.get(url, headers=headers)
 # data = response.text
-html = urllib2.urlopen(url).read()
+html = urllib.urlopen(url).read()
 
 #importing lxml was recommended to parse html rather than just soup = bs4.BeautifulSoup(html)
-soup = BeautifulSoup(html, "lxml")
-for data in soup.findAll('div', {'class': 'ui-widget-content slick-row odd'}):
+soup = BeautifulSoup(html, "html.parser")
+for data in soup.findAll('div', {'class': 'slick-row-odd'}):
     text = data.findChildren()[0].renderContents()
     print text
-
+# .findChildren()[0]
 # info = soup.select('.slick-cell l6 r6')
 #
 # print info
